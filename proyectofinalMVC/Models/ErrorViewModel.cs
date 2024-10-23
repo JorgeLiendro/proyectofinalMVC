6309cdd9d4d108037eacb1e694e9579641cb2ae8
jorgeliendro123 <ronaldocrc630@gmail.com>
2024-10-23 16:09:37 -0400

    Agregar archivos de proyecto.

---
+++ proyectofinalMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyectofinalMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
